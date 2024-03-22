f2af9053cf8a029eea0ea03f2398cd94c7c7cec8
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-03-22 18:15:25 -0500

    Contexto BDContext

--- Data/contexto.cs
+++ Data/contexto.cs
@@ -1,7 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using ModelResidente.Residente;
+using ModelsUser.User;
 
 namespace DataContext.Context;
 public class BDContext : DbContext
 {
 
+    public  BDContext(DbContextOptions<BDContext> options) : base(options)
+    {
+    }
+    //Mapear las tablas en la bd con dbset
+
+    public  DbSet<User> Users{get;set;}
+    public DbSet<Residente> Residentes{get;set;}
 }
